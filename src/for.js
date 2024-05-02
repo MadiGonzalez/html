3adf65186a70d56ad74782b6a06605280dd933da
MadiGonzalez <madi0808hg@gmail.com>
2024-05-02 10:50:05 -0600

    for

---
+++ src/for.js
@@ -0,0 +1,10 @@
+var elemento = document.querySelector('#header');"estoy leyendo arrays"
+
+
+
+for (let index = 0;index <= 10; index++){
+   for (let index2 = 0;index2 <= 10; index2++){
+   var respuesta =index*index2
+    console.log(index, "x",index2,"=",respuesta)
+ }
+}
